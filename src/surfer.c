37d78db90bd317c6cd883b3706553ae8bf076ea9
CraftedCrime MAC <diks.ianferenc@gmail.com>
2021-05-27 23:31:35 +0200

    surfer implemented

---
+++ src/surfer.c
@@ -0,0 +1,5 @@
+//
+// Created by Ian F. Diks on 27.05.21.
+//
+
+#include "surfer.h"
---
+++ src/surfer.h
@@ -0,0 +1,8 @@
+//
+// Created by Ian F. Diks on 27.05.21.
+//
+
+#ifndef IANFD_SURFER_H
+#define IANFD_SURFER_H
+
+#endif //IANFD_SURFER_H
